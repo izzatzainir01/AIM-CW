b264519afd63f691be4e3cff2e51355beef98172
Izzat Z <hcyiz1@nottingham.edu.my>
2022-04-15 01:03:54 +0800

    Modified exampleexperiment.m to automatically format the data in excel

--- exampleexperiment.m
+++ exampleexperiment.m
@@ -15,10 +15,14 @@ maxrestarts = 1e4;        % SET to zero for an entirely deterministic algorithm
 more off;  % in octave pagination is on by default
 
 t0 = clock;
-rand('state', sum(100 * t0));
+rand('state', 696969);
 
 for dim = [2,3,5,10,20]  % small dimensions first, for CPU reasons
+    data_path = sprintf("my_data/dimension_%d.csv", dim);
+    fileID = fopen(data_path, 'w');
+    fprintf(fileID, "Benchmarks,Instance 1,Instance 2,Instance 3,Instance 4,Instance 5,Instance 6,Instance 7,Instance 8,Instance 9,Instance 10,Instance 11,Instance 12,Instance 13,Instance 14,Instance 15,,Scores,Scores,Scores,Scores,Scores,Scores,Scores,Scores,Scores,Scores,Scores,Scores,Scores,Scores,Scores");
     for ifun = benchmarks('FunctionIndices')  % or benchmarksnoisy(...)
+        fprintf(fileID, "\nf%d", ifun);
         for iinstance = [1:15]  % first 15 function instances
             fgeneric('initialize', ifun, iinstance, datapath, opt);
 
@@ -32,15 +36,28 @@ for dim = [2,3,5,10,20]  % small dimensions first, for CPU reasons
                 end
             end
 
+            evaluations = fgeneric('evaluations');
+            delta_ftarget = fgeneric('fbest') - fgeneric('ftarget');
+
+            % Check if new_data is below 1.00e-14 or above 1.00e+03 and adjust values respectively
+            if delta_ftarget < 1.00e-14
+                delta_ftarget = 1.00e-14;
+            elseif delta_ftarget > 1.00e+03
+                delta_ftarget = 1.00e+03;
+            end
+
             fprintf(['f%d in %d-D, instance %d: FEs=%d with %d restarts,' ...
                 ' fbest-ftarget=%.4e, elapsed time [h]: %.2f\n'], ...
                 ifun, dim, iinstance, ...
-                fgeneric('evaluations'), ...
+                evaluations, ...
                 restarts, ...
-                fgeneric('fbest') - fgeneric('ftarget'), ...
+                delta_ftarget, ...
                 etime(clock, t0)/60/60);
 
             fgeneric('finalize');
+
+            % Write data to data.csv
+            fprintf(fileID, ",%.4e", delta_ftarget);
         end
         disp(['      Date and Time: ' num2str(clock, ' %.0f')]);
     end
